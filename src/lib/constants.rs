49b4f197472ad8b2c558c6409c3552b97e693a84
Georgi Angelov <georgi@Georgis-Air.lan>
2021-08-12 13:16:56 +0100

    Progress on mine placement and update dependencies:

--- src/lib/constants.rs
+++ src/lib/constants.rs
@@ -1,3 +1,4 @@
 pub(crate) const BOARD_HEIGHT: usize = 8;
 pub(crate) const BOARD_WIDTH: usize = 8;
+pub(crate) const MINE_PERCENTAGE: u8 = 20;
 
--- src/lib/mineboard.rs
+++ src/lib/mineboard.rs
@@ -1,13 +1,39 @@
-use crate::lib::constants::{BOARD_HEIGHT, BOARD_WIDTH};
+use crate::lib::constants::{BOARD_HEIGHT, BOARD_WIDTH, MINE_PERCENTAGE};
+use rand::Rng;
 
 pub struct MineBoard {
     pub matrix : [[i32; BOARD_HEIGHT]; BOARD_WIDTH]
 }
 
+struct Field {
+    x: u8,
+    y: u8
+}
+
 impl MineBoard {
 
-    pub(crate) fn place_mines(&mut self, x: i32, y: i32) {
-        print!("penis: {}, {}", x, y)
+    pub(crate) fn place_mines(&mut self, x: u8, y: u8) {
+        print!("penis: {}, {}", x, y);
+        let number_of_mines = BOARD_HEIGHT * BOARD_WIDTH * MINE_PERCENTAGE as usize / 100;
+        let mut fields: Vec<Field> = Vec::new();
+
+        // Add possible mines to list
+        for i in 0..BOARD_WIDTH {
+            for j in 0..BOARD_HEIGHT {
+                if num::abs(i - x as usize) > 1 && num::abs(j - y as usize) > 1{
+                    fields.push(Field{ x: i as u8, y: j as u8 });
+                }
+            }
+        }
+
+        // Initialize mines
+        let mut rng = rand::thread_rng();
+        for i in 0..number_of_mines {
+            let field_index = rng.gen(0..fields.len());
+            print!("{}", field_index)
+        }
+
+
     }
 }
 
